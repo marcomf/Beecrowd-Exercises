48948b7792857af2170a0d96dc42306e687ffa33
marcomf <marco.mdf17@gmail.com>
2022-05-29 19:29:48 -0300

    exerc

---
+++ Ex1095C.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int p, i, I, J; 
+
+	for  (p=13, i=1, I=-2, J=65; p >= i; i++)
+    {
+		I = I+3;
+		J = J-5;
+		printf("I=%d J=%d\n",I,J);
+	}
+    return 0;
+}
