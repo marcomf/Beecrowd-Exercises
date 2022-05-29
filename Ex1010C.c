0eea728a352311d294d10b06950a58053a4929ff
marcomf <marco.mdf17@gmail.com>
2022-05-28 21:58:53 -0300

    exerc

---
+++ Ex1010C.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+	int cod, unidades, i;
+	double preco, total=0;
+	
+	for  (i=0; 2>i; i++)
+	{
+		scanf("%d%d%lf",&cod,&unidades,&preco);
+		total = total + unidades*preco;
+	}
+	printf("VALOR A PAGAR: R$ %.2lf\n",total);
+	
+	return 0;
+}
