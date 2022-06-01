5cbfd4b847cbb67ddb2872403f1377e42693e023
marcomf <marco.mdf17@gmail.com>
2022-05-31 23:16:36 -0300

    x

---
+++ Ex1097C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	int I=1, J=8, i, p;
+	for   (i=0,p=1; i<3,p<16; i++,p++)
+    {
+		J=J-1;
+		printf("I=%d J=%d\n",I,J);
+		if(i == 2)
+			i =-1, I=I+2, J=J+5;
+	}
+    return 0;
+}
---
+++ Ex1098C.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	double I=0, J=1, t=0, i, p;
+	
+	for  (i=1,p=1; I<=2; p++,J++)
+    {
+		printf("I=%d J=%d\n",I,J);
+		if  (p==3)
+			I = I+0.2, p = 0, t = t+1, J = 0, J = J+t*0.2;			
+	}
+    return 0;
+}
